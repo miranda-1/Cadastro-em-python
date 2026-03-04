cbbbc2d905925990acbed20a2454ce68d2baefff
Miranda-Dev <joaovictor10_miranda@hotmail.com>
2026-03-04 19:10:37 -0400

    coloquei um dicionario
    adicionei um dicionario no meu sistema de cadastro para eu armazenar as informações

--- cadastro.py
+++ cadastro.py
@@ -1,4 +1,6 @@
 #Menu principal
+pessoas_cadastradas = []
+
 while True:
     print('=' * 20)
     print('CADASTRO INTELIGENTE')
@@ -8,13 +10,13 @@ while True:
     print('2-Ver estatísticas')
     print('3-Sair')
 
-    try:
+    try:#escolha do que vai fazer no sistema
         opcao_selecionada = int(input('Digite qual opçao deseja realizar: '))
     except ValueError:
         print('Digite apenas numeros inteiros')
     else:
-        if opcao_selecionada == 1:
-            while True:
+        if opcao_selecionada == 1:#Cadastrar o nome do usuario
+            while True:#Cadastro do nome
                 print('Você selecionou o cadastro de usúario.')
             
                 cadastro_nome = input('Digite o nome que deseja cadastrar: ')
@@ -27,7 +29,7 @@ while True:
 
 
             while True:
-                try:
+                try:#Cadastro da idade
                     cadastro_idade = int(input('Digite a idade da pessoa cadastrada: '))
                 except ValueError:
                     print('Digite apenas numero inteiros!')
@@ -36,8 +38,23 @@ while True:
                         print('Digite apenas uma idade valida de 0 a 120 anos')
                     else:
                         print('idade do usuario cadastrada!')
+                        clasificação = ''
+                        if 0 <= cadastro_idade < 18:
+                            clasificação = 'Menor de idade'
+                        elif 18 <= cadastro_idade <60:
+                            clasificação = 'Adulto'
+                        else:
+                            clasificação = 'Idoso'   
+                    
                         break
-                
+                        
+            #guardar o cadastro em um dicionario para ter essas informações depois 
+            pessoa = {
+                "Nome": cadastro_nome,
+                "idade": cadastro_idade, 
+                "Caracteristica": clasificação
+            }
+            pessoas_cadastradas.append(pessoa)
 
 
 
@@ -47,6 +64,7 @@ while True:
 
         elif opcao_selecionada == 2:
             print('Você deseja ver as eststística de um usúario')
+            print(f'Todas as pessoas cadastradas: {pessoas_cadastradas}')
         elif opcao_selecionada == 3:
             print('Você selecionou sair do cadastro')
             break
